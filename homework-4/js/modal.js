d22cc04596e6f9bc8ef29513b936400bf31cbde6
Andrii Romanenko <125657585+brilliantic@users.noreply.github.com>
2023-09-18 12:29:33 +0300

    Delete homework-4 directory

--- homework-4/js/modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-open');
-const modalBtnClose = document.querySelector('.modal-close');
-
-const toggleModal = () => modal.classList.toggle('is-hidden');
-
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
